e9b1512327ac656072a7ad5560811cf6a389d847
Keith Baker <github@baker.af>
2023-01-10 23:59:28 -0500

    Parse JSON-formatted text

--- practice/main.go
+++ practice/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "https://services.explorecalifornia.org/json/tours.php"
 
 func main() {
-	fmt.Println("Network requests")
+	fmt.Println("Parse JSON-formatted text")
 
 	resp, err := http.Get(url)
 	checkError(err)
@@ -22,10 +24,37 @@ func main() {
 	content := string(bytes)
 	fmt.Print(content)
 
+	tours := toursFromJson(content)
+	for _, tour := range tours {
+		fmt.Println(tour.Name, " ", tour.Price)
+	}
+
 }
 
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+
+		tours = append(tours, tour)
+	}
+
+	return tours
+}
+
+type Tour struct {
+	Name, Price string
 }
