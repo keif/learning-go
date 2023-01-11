56a07de2754059eb0bd6f0e7c6d3cb13e6923f4d
Keith Baker <github@baker.af>
2023-01-10 22:33:23 -0500

    Evaluate expressions with switch statements

--- practice/main.go
+++ practice/main.go
@@ -2,29 +2,43 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Program conditional logic")
+	fmt.Println("Evaluate expressions with switch statements")
 
-	theAnswer := 42
+	rand.Seed(time.Now().Unix())
+	dow := rand.Intn(7) + 1
+	fmt.Println("Day", dow)
+
 	var result string
-	if theAnswer < 0 {
-		result = "Less than zero"
-	} else if theAnswer == 0 {
-		result = "Equal to zero"
-	} else {
-		result = "Greater than zero"
-	}
-	fmt.Println(result)
-
-	if x := -42; x < 0 {
-		result = "Less than zero"
-	} else if x == 0 {
-		result = "Equal to zero"
-	} else {
-		result = "Greater than zero"
+	switch dow {
+	case 1:
+		result = "It's Sunday"
+		// fallthrough
+	case 2:
+		result = "It's Monday"
+		// fallthrough
+	case 3:
+		result = "It's Tuesday"
+		// fallthrough
+	case 4:
+		result = "It's Wednesday"
+		// fallthrough
+	case 5:
+		result = "It's Thursday"
+		// fallthrough
+	case 6:
+		result = "It's Friday"
+		// fallthrough
+	case 7:
+		result = "It's Saturday"
+		// fallthrough
+	default:
+		result = "It's some other day!"
 	}
 	fmt.Println(result)
 
